f308e89731d37c0fb7621eead5f0e5ef60dc715e
Filip Plachta <plachtaf@gmail.com>
2022-01-01 17:44:20 +0000

    Master

--- src/main/java/com/polsl/clinicservicesystem/repository/RoleRepository.java
+++ src/main/java/com/polsl/clinicservicesystem/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package com.polsl.clinicservicesystem.repository;
 
 import com.polsl.clinicservicesystem.model.RoleEntity;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<RoleEntity, String> {

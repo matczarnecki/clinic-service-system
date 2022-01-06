00b191ca412db1931db6f3665fc6cd5cae5d5e36
Filip Plachta <filip.plachta@softiq.pl>
2022-01-06 18:27:04 +0100

    Poprawa stopki

--- src/frontend/src/ui/Footer.jsx
+++ src/frontend/src/ui/Footer.jsx
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core';
 const Footer = () => (
   <div className="footer">
     <Typography variant="overline" style={{float: 'right', marginRight: '1%'}}>
-      Wersja: {process.env.REACT_APP_NOT_SECRET_CODE}
+      Wersja: {process.env.REACT_APP_VERSION}
     </Typography>
   </div>
 );
